df6379eb975ed6cc8e9ec06c5932e271dbb6bbcc
abhisheksafui <abhishek6590@gmail.com>
2017-01-06 23:47:35 +0530

    compiled

--- iotScheduler/include/load_config.h
+++ iotScheduler/include/load_config.h
@@ -1,4 +1,4 @@
-Copyright 2016 Abhishek Safui
+/*Copyright 2016 Abhishek Safui
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -12,7 +12,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+*/
 #ifndef LOAD_CONF_H
 #define LOAD_CONF_H
 
